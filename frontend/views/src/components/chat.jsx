f02de01e9e74ff6c463a3e4bf301ba24738a6ac1
Kaue Santos <kaue.santoscruz04@gmail.com>
2024-02-12 15:56:54 -0300

    commit

--- frontend/views/src/components/chat.jsx
+++ frontend/views/src/components/chat.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function chat(){
-    return{
-        <div class="chats">
+export default function chat() {
+    return {
+        <div className="chats">
             <div class="chat">
                 <div class="chat-body">
                     <div class="chat-content" id="chat-content">
