2458f4f5529abd705bef59ae1a766d47052ea95c
Brenda <brendalopes.unicentro@gmail.com>
2024-02-12 16:22:00 -0300

    xcz

---
+++ frontend/views/src/components/chat/chat.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Chat() {
+  return (
+    <div className="chats">
+      <div className="chat">
+        <div className="chat-body">
+          <div className="chat-content" id="chat-content">
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
