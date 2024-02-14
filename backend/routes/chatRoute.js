6c1e90304524a5878493a5cacc414a53d207308b
KWSantos <141880149+KWSantos@users.noreply.github.com>
2024-02-14 16:52:06 -0300

    commit

--- backend/routes/chatRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const chatRouter = express.Router()
-
-const chatController = require('../controllers/chatController')
-
-chatRouter.get('/', chatController.getChat)
-chatRouter.post('/loadResponse', chatController.postLoadResponse)
-
-module.exports = chatRouter 
--- backend/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const userRouter = express.Router()
-
-const userController = require('../controllers/userController')
-
-userRouter.post('/sendMessage', userController.postSendMessage)
-userRouter.post('/loadMessage', userController.postLoadMessage)
-
-module.exports = userRouter
