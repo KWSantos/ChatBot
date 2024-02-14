1c324a0e1e88741e41370b61a0fcd219f6b088eb
Kaue Santos <kaue.santoscruz04@gmail.com>
2024-02-14 16:46:35 -0300

    commit

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,5 +4,5 @@ const userRouter = express.Router()
 const userController = require('../controllers/userController')
 
 userRouter.post('/sendMessage', userController.postSendMessage)
-
+//comentario ne porra
 module.exports = userRouter
